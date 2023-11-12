532faa8aa07cc4cb4b891b10c4fa20d59abe8e6e
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2023-11-12 17:40:58 +0200

    final

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public Health player1;
+    public Health player2;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (player1.hp == 0 || player2.hp == 0)
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
